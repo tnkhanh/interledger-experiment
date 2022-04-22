a6a81648aa2c2365aab7b2bed7dbaf8a3d8d2b8b
TN Khanh <tnkhanh4@gmail.com>
2022-04-22 16:24:13 +0800

    Add log and result for amount experiment

--- exp.py
+++ exp.py
@@ -18,14 +18,20 @@ def amount_experiment():
     N = 200 
     times = np.zeros(N) 
     sums = np.zeros(N)
-    amt = 0
+    amt = 10000
+
+    f = open("amount_report.txt", "w")
     for i in range(N):
         command = alice_cli + " pay alice --auth alice_password --amount " + str(amt) + " --to http://charlie-node:7770/accounts/charlie/spsp"
         start_time = time.time()
         subprocess.call(command, shell=True, stdout=subprocess.DEVNULL)
         end_time = time.time()
         elapsed = end_time - start_time
-        print("Amount: " + str(amt) + ', elapsed: ' + str(elapsed))
+        report = "Amount: " + str(amt) + ', elapsed: ' + str(elapsed)
+        print(report)
+        f.write(report)
+        f.write("\n")
+
         times[i] = elapsed
         if i==0:
             sums[i] = times[i]
@@ -33,23 +39,29 @@ def amount_experiment():
             sums[i] = sums[i-1] + times[i]
         amt += 10000
 
-    x = np.linspace(0, 2000 * N, N)
-#    fig, ax = plt.subplots()
-#    ax.plot(x, sums)
-#    ax.plot(x, times)
+    amounts = np.linspace(10000, 10000 * N, N)
 
-    plt.subplot(1, 2, 1)
-    plt.plot(x, times)
+    fig, ax = plt.subplots()
+    ax.plot(amounts, times)
     plt.title("Amount-time chart")
     plt.xlabel("Amount(gwei)")
     plt.ylabel("Time(s)")
-
-    plt.subplot(1, 2, 2)
-    plt.plot(x, sums)
-    plt.title("Total time chart")
-    plt.xlabel("Amount(gwei)")
-    plt.ylabel("Total time(s)")
+    plt.savefig("amount.png", bbox_inches="tight")
     plt.show()
+
+    log = pd.DataFrame({'Amount': amounts, 'Time': times})
+    log.to_csv('amount_log.csv', index = False)
+#    ax.plot(x, times)
+
+#    plt.subplot(1, 2, 1)
+#    plt.plot(x, times)
+#
+#    plt.subplot(1, 2, 2)
+#    plt.plot(x, sums)
+#    plt.title("Total time chart")
+#    plt.xlabel("Amount(gwei)")
+#    plt.ylabel("Total time(s)")
+#    plt.show()
 
 
 command = alice_cli + " pay alice --auth alice_password --amount 20000 --to http://charlie-node:7770/accounts/charlie/spsp"
@@ -85,7 +97,6 @@ def throughput_experiment():
         times[i] = elapsed
 
     f.close()
-
     x = np.linspace(0, N, N)
 
     fig, ax = plt.subplots()
@@ -96,8 +107,21 @@ def throughput_experiment():
     plt.savefig("interledger.png", bbox_inches="tight")
     plt.show()
 
+def plot_amount_log():
+    df = pd.read_csv('amount_log.csv')
+    amounts = df[:]['Amount'].to_numpy()
+    times = df[:]['Time'].to_numpy()
+
+    fig, ax = plt.subplots()
+    ax.plot(amounts, times)
+    plt.title("Amount-time chart")
+    plt.xlabel("Amount(gwei)")
+    plt.ylabel("Time(s)")
+    plt.show()
+
 if __name__ == "__main__":
-    throughput_experiment()
+#    amount_experiment()
+    plot_amount_log()
 
 #    info = resource.getrusage(resource.RUSAGE_CHILDREN)
 #    print("real " + str(elapsed))
